283e7c86d09e2286163e327b83f29380b96b8fba
edavidaja <self@edavidaja.com>
2021-12-10 18:22:01 -0500

    rdp

---
+++ Casks/rstudio-pro-daily.rb
@@ -0,0 +1,22 @@
+cask "rstudio-pro-daily" do
+  version :latest
+  sha256 :no_check
+
+  url "https://rstudio.org/download/latest/daily/desktop/mac/RStudio-latest.dmg",
+  name "RStudio Pro"
+  desc "RStudio is an integrated development environment (IDE) for R."
+  homepage "https://www.rstudio.com/"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "RStudio Pro.app"
+
+  zap trash: "~/.rstudio-desktop"
+
+  caveats <<~EOS
+    #{token} depends on R. The R Project provides official binaries:
+      brew install --cask r
+    Alternatively, the Homebrew-compiled version of R omits the GUI app:
+      brew install r
+  EOS
+end
