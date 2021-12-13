df1d7a9e7ebf6c5652f49a08bdcea3644405bc13
edavidaja <self@edavidaja.com>
2021-12-13 09:50:43 -0500

    rename cask

--- Casks/rstudio-pro.rb
+++ Casks/rstudio-pro.rb
@@ -1,11 +1,10 @@
-cask "rstudio" do
+cask "rstudio-pro" do
   version :latest
   sha256 :no_check
 
   url "https://rstudio.org/download/latest/daily/desktop/mac/RStudio-pro-latest.dmg",
       verified: "https://rstudio.org/"
   name "RStudio Pro"
-  desc "Integrated development environment for R"
   homepage "https://www.rstudio.com/"
 
   conflicts_with cask: "rstudio"
@@ -15,10 +14,4 @@ cask "rstudio" do
 
   zap trash: "~/.rstudio-desktop"
 
-  caveats <<~EOS
-    #{token} depends on R. The R Project provides official binaries:
-      brew install --cask r
-    Alternatively, the Homebrew-compiled version of R omits the GUI app:
-      brew install r
-  EOS
 end
